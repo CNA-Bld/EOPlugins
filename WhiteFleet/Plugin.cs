7dd4fb0fc1ec26d822efb34c955bcc719b2521a6
CNA-Bld <cna.bld@gmail.com>
2016-07-02 13:40:27 +0800

    Implement WhiteFleet

---
+++ WhiteFleet/Plugin.cs
@@ -0,0 +1,34 @@
+﻿using ElectronicObserver.Window.Plugins;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ElectronicObserver.Window;
+using ElectronicObserver.Notifier;
+
+namespace WhiteFleet
+{
+    public class Plugin : ServerPlugin
+    {
+        public override string MenuTitle
+        {
+            get
+            {
+                return "ホワイト艦隊";
+            }
+        }
+
+        public override bool RunService(FormMain main)
+        {
+            Task.Factory.StartNew(() => WhiteObserver.Instance.Initialize());
+
+            return true;
+        }
+
+        public override PluginSettingControl GetSettings()
+        {
+            return null;
+        }
+    }
+}
